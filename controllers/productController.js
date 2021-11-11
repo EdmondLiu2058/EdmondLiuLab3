71210f9b1457f2feb5bd65ec25a2ae8ff9d45cbb
EdmondLiu2058 <89442574+EdmondLiu2058@users.noreply.github.com>
2021-11-12 02:01:16 +0800

    article established

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,31 +1,32 @@
+//for catalogue.ejs show catalog table
 const getCatalogue = (request, response) => {
-    const catalogServices = require('../services/productServices');
-    catalogServices.searchService(function(err, rows) {
-        response.json(rows);
-        response.end();
-    });
+  const catalogServices = require("../services/productServices");
+  catalogServices.searchService(function (err, rows) {
+    response.render("catalogue", { products: rows });
+  });
 };
 
+//for article.ejs
 const getProductByID = (request, response) => {
-    const catalogServices = require('../services/productServices');
-    let reference = request.params.reference;
-    catalogServices.searchIDService(reference, function(err, rows) {
-        response.json(rows);
-        response.end();
-    });
+  const catalogServices = require("../services/productServices");
+  let reference = request.params.reference;
+  catalogServices.searchIDService(reference, function (err, rows) {
+    response.json(rows);
+    response.end();
+  });
 };
 
 const getProductsByCategory = (request, response) => {
-    const catalogServices = require('../services/productServices');
-    let reference = request.params.category;
-    catalogServices.searchCategoryService(category, function(err, rows) {
-        response.json(rows);
-        response.end();
-    });
+  const catalogServices = require("../services/productServices");
+  let reference = request.params.category;
+  catalogServices.searchCategoryService(category, function (err, rows) {
+    response.json(rows);
+    response.end();
+  });
 };
 
 module.exports = {
-    getCatalogue,
-    getProductByID,
-    getProductsByCategory
+  getCatalogue,
+  getProductByID,
+  getProductsByCategory
 };
